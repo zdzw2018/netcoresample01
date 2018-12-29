748b7aec98dafe4642607100dfdde418b155a964
zdzw2018 <1029667353@qq.com>
2018-12-29 14:54:24 +0800

    新增第三篇文章的事例代码

---
+++ Sample02/Sample01/Controllers/ContentController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Sample01.Models;
+
+namespace Sample01.Controllers
+{
+    /// <summary>
+    /// Content控制器
+    /// </summary>
+    public class ContentController : Controller
+    {
+        private readonly Content contents1;
+        public ContentController(IOptionsSnapshot<Content> option)
+        {
+            contents1 = option.Value;
+        }
+
+        /// <summary>
+        /// 首页显示
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            var contents = new List<Content>();
+            //for (int i = 0; i < 11; i++)
+            //{
+            //    contents.Add(new Content { Id = i, title = $"{i}的标题", content = $"{i}的内容", status = 1, add_time = DateTime.Now.AddDays(-i) });
+            //}
+            contents.Add(contents1);
+            return View(new ContentViewModel { Contents = contents });
+        }
+    }
+}
---
+++ Sample02/Sample01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Sample01
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                var env = hostingContext.HostingEnvironment;
+                config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                .AddJsonFile("Content.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables();
+            })
+                .UseStartup<Startup>();
+    }
+}
